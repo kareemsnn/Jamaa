72baeb0a992e8d91b4fd5bfd2b644fe3bd1cae83
kareemsnn <kareemsnn@gmail.com>
2024-07-26 02:04:33 -0400

    tokens and Auth

--- frontend/api.js
+++ frontend/api.js
@@ -1,6 +1,24 @@
 import axios from 'axios';
+import { ACCESS_TOKEN } from './constants';
 
-const API_URL = 'http://127.0.0.1:8000/MosqueApp/';
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
+})
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+
 
 export const getAllPosts = async () => {
   try {
@@ -41,3 +59,5 @@ export const loginUser = async (userData) => {
     throw error;
   }
 };
+
+export default api;
